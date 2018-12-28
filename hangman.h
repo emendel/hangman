4fab12e5424f867c185b6b4d7ea6472816f7e98e
Ezra Mendelson <emendel@umich.edu>
2018-12-28 16:03:14 -0500

    Separate cpp and header

---
+++ hangman.h
@@ -0,0 +1,63 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+using namespace std;
+
+void play_hangman(string word);
+
+void play_hangman(string word){
+    map <char, int> guesses;
+    vector <char> output;
+    map <char, int> distinct;
+    for (int i = 0; i < word.length(); ++i){
+        output.push_back('_');
+        if (distinct.find(word[i]) == distinct.end()){
+            distinct.insert({word[i], 1});
+        }
+    }
+
+    for (auto i : output){
+        cout << i << " ";
+    }
+
+    cout << endl << "Guess a letter!" << endl;
+    int num_guessed_correctly = 0;
+    int misses;
+    char letter;
+    while (num_guessed_correctly != distinct.size()){
+        cout << "You are allowed " << 10 - misses << " more incorrect guesses" << endl;
+        cin >> letter;
+        if (guesses.find(letter) != guesses.end()){
+            cout << "Sorry you already guessed that letter" << endl;
+        }
+        else if (distinct.find(letter) == distinct.end()){
+            ++misses;
+            if (misses == 10){
+                cout << "Guesser lost :(" << endl;
+                return;
+            }
+
+            cout << "Sorry try again :(" << endl;
+        }
+
+        else {
+            for (int i = 0; i < word.length(); ++i){
+                if (word[i] == letter){
+                    output.at(i) = letter;
+                }
+            }
+            ++num_guessed_correctly;
+            cout << "Great job!  Guess another letter!" << endl;
+        }
+        guesses.insert({letter, 0});
+
+        for (auto i : output){
+            cout << i << " ";
+        }
+        cout << endl;
+    
+    }
+    cout << "Guesser Won!!" << endl;
+
+}
